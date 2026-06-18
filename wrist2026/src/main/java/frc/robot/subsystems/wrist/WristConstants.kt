d0305998f547926c8d3b2044e6f6515ef7e4da8b
lucas <lucas.zeng.2021@gmail.com>
2026-06-18 19:31:38 -0400

    Added AutoLogs, signals, and implemented more of wrist

---
+++ wrist2026/src/main/java/frc/robot/subsystems/wrist/WristConstants.kt
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.wrist
+
+object WristConstants {
+    const val WRIST_MOTOR_ID = 0
+
+    const val WRIST_ANGLE_ERROR_TOLERANCE = 1.0
+}
--- wrist2026/src/main/java/frc/robot/subsystems/wrist/WristIOHardware.kt
+++ wrist2026/src/main/java/frc/robot/subsystems/wrist/WristIOHardware.kt
@@ -1,26 +1,66 @@
 package frc.robot.subsystems.wrist
 
-open class WristIOHardware() : WristIO {
+import com.ctre.phoenix6.BaseStatusSignal
+import com.ctre.phoenix6.controls.PositionVoltage
+import com.ctre.phoenix6.controls.VelocityVoltage
+import com.ctre.phoenix6.controls.VoltageOut
+import com.ctre.phoenix6.hardware.TalonFX
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Angle
+import edu.wpi.first.units.measure.AngularVelocity
 
-    override var currentAngle: Double
-        get() = TODO("Not yet implemented")
-        set(value) {}
+open class WristIOHardware : WristIO {
 
-    override var targetAngle: Double
-        get() = TODO("Not yet implemented")
-        set(value) {}
+    val wristMotor = TalonFX(WristConstants.WRIST_MOTOR_ID)
 
-    override fun setAngle(angle: Double) {
-        targetAngle = angle
+    private val wristVoltageRequest = VoltageOut(0.0)
+    private val wristVelocityRequest = VelocityVoltage(0.0)
+    private val wristAngleRequest = PositionVoltage(0.0)
+
+    private val wristMotorVoltage = wristMotor.motorVoltage
+    private val wristMotorVelocity = wristMotor.velocity
+    private val wristMotorSupplyCurrent = wristMotor.supplyCurrent
+    private val wristMotorStatorCurrent = wristMotor.statorCurrent
+    private val wristMotorTemp = wristMotor.deviceTemp
+
+    private val isWristMotorConnected: Boolean
+        get() = BaseStatusSignal.isAllGood(
+            wristMotorVoltage,
+            wristMotorVelocity,
+            wristMotorStatorCurrent
+        )
+
+    init {
+
+    }
+
+    override fun updateInputs(inputs: WristIO.WristIOInputs) {
+        inputs.wristConnected = isWristMotorConnected
+        inputs.wristAppliedVolts = wristMotorVoltage.value.`in`(Units.Volts)
+        inputs.pitchVelocityRadsPerSec = wristMotorVelocity.value.`in`(Units.RadiansPerSecond)
+        //pitch
+        //inputs.wristPitch += wristMotorVelocity.value * deltaTime?????
+        inputs.wristSupplyCurrentAmps = wristMotorSupplyCurrent.value.`in`(Units.Amps)
+        inputs.wristStatorCurrentAmps = wristMotorStatorCurrent.value.`in`(Units.Amps)
+        inputs.wristTempCelsius = wristMotorTemp.value.`in`(Units.Celsius)
+    }
+
+    override fun setWristVoltage(voltage: Double) {
+        wristMotor.setControl(wristVoltageRequest.withOutput(voltage))
+    }
+
+    override fun setWristVelocity(velocity: AngularVelocity) {
+        wristMotor.setControl(wristVelocityRequest.withVelocity(velocity))
+    }
+
+    override fun setWristAngle(angle: Angle) {
+        wristMotor.setControl(wristAngleRequest.withPosition(angle))
     }
 
     override fun runCommandOne(angle: Double) {
-        setAngle(angle)
+        //setWristAngle(Angle.of(angle))
     }
 
     override fun runCommandTwo(angleOne: Double, angleTwo: Double) {
-        setAngle(angleOne)
-        // wait one second
-        setAngle(angleTwo)
     }
 }
