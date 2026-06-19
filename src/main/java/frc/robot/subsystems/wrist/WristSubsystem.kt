704abba9c068839a1e99d70ff873c9aa51729694
lucas <lucas.zeng.2021@gmail.com>
2026-06-19 14:45:32 -0400

    Updated to include necessary packages, settings, and also moved the commands to subsystem

--- src/main/java/frc/robot/subsystems/wrist/WristSubsystem.kt
+++ src/main/java/frc/robot/subsystems/wrist/WristSubsystem.kt
@@ -1,10 +1,14 @@
 package frc.robot.subsystems.wrist
 
+import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.wpilibj.smartdashboard.Field2d
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.littletonrobotics.junction.AutoLog
 import org.littletonrobotics.junction.AutoLogOutput
 import edu.wpi.first.wpilibj.Alert
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.WaitCommand
 import org.littletonrobotics.junction.Logger
 import kotlin.math.abs
 
@@ -40,4 +44,14 @@ class WristSubsystem (
 
         return error < WristConstants.WRIST_ANGLE_ERROR_TOLERANCE
     }
+
+    fun runCommandOne(angle: Angle): Command =
+        runOnce { io.setWristAngle(angle) }
+
+    fun runCommandTwo(angleOne: Angle, angleTwo: Angle): Command =
+        Commands.sequence(
+            runOnce { io.setWristAngle(angleOne) },
+            WaitCommand(1.0),
+            runOnce { io.setWristAngle(angleTwo) }
+        )
 }
